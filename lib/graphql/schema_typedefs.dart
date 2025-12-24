b719533f6b84cf795c919c95ae863e9b5ac6d22f
Rick Krystianne Lim <rickimaru18@gmail.com>
2025-12-24 10:27:41 +0800

    feat: add GraphQL schema typedefs

---
+++ lib/graphql/schema_typedefs.dart
@@ -0,0 +1,4 @@
+import 'package:samansa_flutter_test/graphql/query/trailerVideos.graphql.dart';
+
+typedef Trailer = Query$trailerVideos$trailerVideos$edges;
+typedef TrailerNode = Query$trailerVideos$trailerVideos$edges$node;
